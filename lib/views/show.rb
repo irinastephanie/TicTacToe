a57b93627fb0a42d0e8980a1c2c8dca5e3f9f085
irinastephanie <52470627+irinastephanie@users.noreply.github.com>
2019-07-25 23:39:31 +0200

    Update show.rb

--- lib/views/show.rb
+++ lib/views/show.rb
@@ -5,6 +5,6 @@ def show_board(board)
         puts "#{@boardcase[3..5].join(" | ")}"
         puts "--|---|--"
         puts "#{@boardcase[6..8].join(" | ")}"
-    #TO DO : affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
+    #Affiche sur le terminal l'objet de classe Board en entrée. S'active avec un Show.new.show_board(instance_de_Board)
 end
 end

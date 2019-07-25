d193a7a9dad4e40b506d7614f9a56eb8aa953d8d
irinastephanie <52470627+irinastephanie@users.noreply.github.com>
2019-07-25 23:10:19 +0200

    Update player.rb

---
+++ lib/app/player.rb
@@ -0,0 +1,12 @@
+class Player
+attr_reader :name, :symbol #attribut d'un joueur
+
+    def initialize(name, symbol)
+    @name = name.to_s 
+    @symbol = symbol #determiner sur quel symbole (X, O) le joueur va jouer
+    end
+    def welcome
+        puts "Le jouer #{name} joue avec le symbole #{symbol}!"
+    end 
+end
+#comment la personne joue(afilié a game)

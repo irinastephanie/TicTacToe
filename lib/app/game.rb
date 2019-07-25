df69a251d0e7e36cee79faf3541c92910f0d3db5
irinastephanie <52470627+irinastephanie@users.noreply.github.com>
2019-07-25 23:37:31 +0200

    Update game.rb

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -7,7 +7,7 @@ class Game #le jeu, initialise partie, joue un tour, fin de partie
         @board = board
         @status = "on going"
         @current_player = ""
-end
+    end
 
 
     def turn(board)
@@ -16,7 +16,7 @@ end
         if boardcase > 8 || boardcase < 0
           puts "Le chiffre doit être entre 0 et 8"
         elsif @current_player.choose_boardcase(boardcase) != false
-          @victory = @current_player if @current_player.victory?
+          @wins = @current_player if @current_player.wins?
           @turn += 1
           switch_player
         #méthode faisant appelle aux méthodes des autres classes (notamment à l'instance de Board). Elle affiche le plateau, demande au joueur ce qu'il joue, vérifie si un joueur a gagné, passe au joueur suivant si la partie n'est pas finie.
@@ -24,11 +24,18 @@ end
     def switch_player
         @current_player = @current_player == @player1 ? @player2 : @player1
       end
-    def new_round
-    # TO DO : relance une partie en initialisant un nouveau board mais en gardant les mêmes joueurs.
-    end
     def game_end
     # TO DO : permet l'affichage de fin de partie quand un vainqueur est détecté ou si il y a match nul
+    Board.new(@board)
+      if @board.win == @player1.symbol
+        player1.wins += 1
+        puts " #{@player1.name} a gagné!"
+      elsif @board.win == @player2.symbol
+        player2.wins += 1
+        puts "C'est #{@player2.name} qui a gagné!"
+      else
+        puts "Egalité!""
+      end
     end    
-    end
+end
     
